f99becf3cad5de8834bc580cba1ef056e2fd2200
Anil Sarioglu <s109345@ap.be>
2020-07-20 19:48:00 +0200

    Installed automapper and configured it

---
+++ MusicCollection/BLL/autoMapper/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace BLL.autoMapper
+{
+    internal class Mapper
+    {
+        internal static TDestination Map<TSource, TDestination>(TSource objectToMap)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<TDestination>(objectToMap);
+        }
+    }
+}
